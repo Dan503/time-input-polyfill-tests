f7fdf35e6498de875e58685ab85f3fc2e6aa81e4
Daniel Tonon <dan503.npm@gmail.com>
2022-02-26 16:08:39 +1100

    Cleaning up the utils file

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,7 +1,6 @@
 import type { Segment } from "@time-input-polyfill/utils"
-import { toLeadingZero } from "@time-input-polyfill/utils"
+import { toLeadingZero, a11yID } from "@time-input-polyfill/utils"
 import { loadTestPage, LoadTestPageParams } from "./loadTestPage"
-import { label, testId } from '../../src/TestComponent'
 
 type GetCyElem = () => Cypress.Chainable<JQuery<HTMLElement>>
 
@@ -42,7 +41,7 @@ export class Utils {
 		this.IDs = {
 			primaryTestsId: primaryTestsLabel.toLowerCase().replaceAll(' ', '-'),
 			eventTestsId: eventTestsLabel.toLowerCase().replaceAll(' ', '-'),
-			a11yId: a11yId,
+			a11yId: a11yID,
 		}
 		this.use = {
 			upArrow: () => this.cyInput().trigger('keydown', { key: 'ArrowUp' }).trigger('keyup', { key: 'ArrowUp' }).wait(10),
@@ -116,98 +115,14 @@ export class Utils {
 		const regex = /(\d\d):(\d\d) ([AP]M)/i
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		const [_match, hrs12, minutes, mode] = regex.exec(string12hr) || []
-		const inputSelection = cySelectSegment('hrs12').type(`${hrs12}${minutes}${mode}`)
+		const inputSelection = this.cySelectSegment('hrs12').type(`${hrs12}${minutes}${mode}`)
 
 		const targets = {
-			hrs12: () => inputSelection.then(use.leftArrow).then(use.leftArrow),
-			minutes: () => inputSelection.then(use.leftArrow),
+			hrs12: () => inputSelection.then(this.use.leftArrow).then(this.use.leftArrow),
+			minutes: () => inputSelection.then(this.use.leftArrow),
 			mode: () => inputSelection,
 		}
 
 		return targets[finishingSegment]()
 	}
-}
-
-export const forcedPolyfillId = testId
-export const _forcedPolyfillId = `#${forcedPolyfillId}`
-
-// TODO: this should be exported from "@time-input-polyfill/utils", not stored locally here
-export const a11yId = 'time-input-polyfill-accessibility-block'
-export const _a11yId = `#${a11yId}`
-
-export const cyInput = () => cy.get(_forcedPolyfillId)
-export const cyA11y = () => cy.get(_a11yId)
-export const $input = (jQueryInput: JQuery<HTMLElement>) => jQueryInput[0] as HTMLInputElement
-
-export const hasReturnVal = (expectation: string) => () => cy.get(`#${testId}-return-value`).wait(10).should('have.text', expectation).then(cyInput)
-
-interface A11yInitialHtmlReturn {
-	hrs12: string
-	minutes: string,
-	mode: string,
-	focus: string
-}
-
-export const a11yInitialHtml = (value?: number | string): A11yInitialHtmlReturn => {
-	const hrs12 = `<p>Hours spin button ${value || 8}.</p>`
-	return ({
-		hrs12,
-		minutes: `<p>Minutes spin button ${value || 30}.</p>`,
-		mode: `<p>AM/PM spin button ${value || 'PM'}.</p>`,
-		focus: `<p>${label} grouping ${toLeadingZero(value || 8)}:30 PM.</p>` + hrs12
-	})
-}
-
-export const a11yHasExpectedHtml = (expectedHtml: string) => () => cyA11y().wait(10).should('have.html', expectedHtml).then(cyInput)
-
-export const sendFocus = () => {
-	return cyInput().focus().wait(100)
-}
-
-export const setTime = (string12hr: string, finishingSegment: Segment = 'hrs12') => {
-	const regex = /(\d\d):(\d\d) ([AP]M)/i
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const [_match, hrs12, minutes, mode] = regex.exec(string12hr) || []
-	const inputSelection = cySelectSegment('hrs12').type(`${hrs12}${minutes}${mode}`)
-
-	const targets = {
-		hrs12: () => inputSelection.then(use.leftArrow).then(use.leftArrow),
-		minutes: () => inputSelection.then(use.leftArrow),
-		mode: () => inputSelection,
-	}
-
-	return targets[finishingSegment]()
-}
-
-export const use = {
-	upArrow: () => cyInput().trigger('keydown', { key: 'ArrowUp' }).trigger('keyup', { key: 'ArrowUp' }).wait(10),
-	downArrow: () => cyInput().trigger('keydown', { key: 'ArrowDown' }).trigger('keyup', { key: 'ArrowDown' }).wait(10),
-	leftArrow: () => cyInput().trigger('keydown', { key: 'ArrowLeft' }).trigger('keyup', { key: 'ArrowLeft' }).wait(10),
-	rightArrow: () => cyInput().trigger('keydown', { key: 'ArrowRight' }).trigger('keyup', { key: 'ArrowRight' }).wait(10),
-	// tab: () => cyInput().tab(),
-	// shiftTab: () => cyInput().tab({ shift: true }),
-	del: () => cyInput().trigger('keydown', { key: 'Delete' }).trigger('keyup', { key: 'Delete' }).wait(10),
-	backspace: () => cyInput().trigger('keydown', { key: 'Backspace' }).trigger('keyup', { key: 'Backspace' }).wait(10)
-}
-
-export const cySelectSegment = (segmentToEndOn: Segment) => {
-	const inputSelection = sendFocus()
-
-	const targets = {
-		hrs12: () => inputSelection,
-		minutes: () => inputSelection.then(use.rightArrow),
-		mode: () => inputSelection.then(use.rightArrow).then(use.rightArrow),
-	}
-
-	return targets[segmentToEndOn]().wait(100)
-}
-
-export const clearAllSegments = (segmentToEndOn: Segment) => {
-	return cyInput().focus().type('{del}').should('have.value', '--:30 PM')
-		.then(() => use.rightArrow())
-		.then(() => cyInput().type('{del}').should('have.value', '--:-- PM'))
-		.then(() => use.rightArrow())
-		.then(() => cyInput().type('{del}').should('have.value', '--:-- --'))
-		.then(() => cyInput().blur())
-		.then(() => cySelectSegment(segmentToEndOn))
 }
