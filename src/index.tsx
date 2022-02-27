5382bddba1f110f81b3e02a7358cd97c77cc11e2
Daniel Tonon <dan503.npm@gmail.com>
2022-02-27 14:40:37 +1100

    Deleting unecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { TestComponent } from './TestComponent'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+	<React.StrictMode>
+		<TestComponent />
+	</React.StrictMode>,
+	document.getElementById('root'),
+)
